702ca7d4edf6eca6ec4967f2eed3585244d562c4
David van IJzendoorn <davidvanijzendoorn@gmail.com>
2024-04-30 17:12:47 +0200

    fix versioning issues

--- src/ImmunoViewer/_version.py
+++ src/ImmunoViewer/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.6.dev27+g14a9953.d20240430'
-__version_tuple__ = version_tuple = (0, 1, 6, 'dev27', 'g14a9953.d20240430')
+__version__ = version = '0.1.6.dev28'
+__version_tuple__ = version_tuple = (0, 1, 6, 'dev28')
