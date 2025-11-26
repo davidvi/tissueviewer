59635a2e0cdd0947f702d5b51c0c77634dbb3656
David van IJzendoorn <davidvanijzendoorn@gmail.com>
2025-11-26 09:53:16 +0100

    set for mini

--- server/run_server_local.sh
+++ server/run_server_local.sh
@@ -1,7 +1,6 @@
-export IV_SLIDE_DIR=/Users/vanijzen/Desktop/iv-store
-# export IV_SLIDE_DIR=/Volumes/DATA/iv-store
-export IV_IMPORT_DIR=/Users/vanijzen/Desktop/iv-import
-export IV_TMP_DIR=/tmp
+export TV_SLIDE_DIR=/Volumes/solid-data2/iv-store3
+export TV_IMPORT_DIR=/Volumes/solid-data2/iv-import2
+export TV_TMP_DIR=/tmp
 
 # python server.py --reload
 gunicorn --workers 10 --threads 10 -b :9000 -k uvicorn.workers.UvicornWorker --reload server:app
